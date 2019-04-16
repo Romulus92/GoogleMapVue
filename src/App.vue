e1e966eeaa766476643da5ec0fb620957c2d2148
Roman Zaynullin <r0mulusss@yandex.ru>
2019-04-16 21:05:41 +0500

    added deploy to gh pages

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <Map></Map>
+    <GmapMap
+      :center="{lat: 54.713471, lng: 55.994398}"
+      :zoom="12"
+      map-type-id="roadmap"
+      style="width: 800px; height: 600px"
+    >
+      <GmapMarker
+        :position="{lat: 54.713471, lng: 55.994398}"
+      />
+    </GmapMap>
   </div>
 </template>
 
 <script>
-import Map from './components/Map.vue'
-
 export default {
-  name: 'app',
-  components: {
-    Map
-  }
+  name: 'app'
 }
 </script>
 
