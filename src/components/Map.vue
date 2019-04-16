f015b96b2106ef9625435f178d0bfc90123ed5f9
Roman Zaynullin <r0mulusss@yandex.ru>
2019-04-16 20:14:19 +0500

    initial commit

---
+++ src/components/Map.vue
@@ -0,0 +1,23 @@
+<template>
+  <GmapMap
+  :center="{lat: 54.713471, lng: 55.994398}"
+  :zoom="12"
+  map-type-id="roadmap"
+  style="width: 800px; height: 600px"
+>
+  <GmapMarker
+    :position="{lat: 54.713471, lng: 55.994398}"
+  />
+</GmapMap>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
+
